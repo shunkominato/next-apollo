8e61581f8c9b8dc2682b918ecc9291f824c10b63
shunkominato <shun19871001@gmail.com>
2023-05-04 11:46:05 +0900

    d

--- src/graphql/generated/graphql.tsx
+++ src/graphql/generated/graphql.tsx
@@ -13,20 +13,138 @@ export type Scalars = {
   Boolean: boolean;
   Int: number;
   Float: number;
+  Timestamp: any;
+};
+
+export type CreateTodoInput = {
+  todo: Scalars['String'];
+  todoStatusId: Scalars['Int'];
+  userId: Scalars['Int'];
+};
+
+export type CreateTodoStatusInput = {
+  todoStatus: Scalars['String'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createTodo: Todo;
+  createTodoStatus: TodoStatus;
+  deleteTodo: Scalars['Boolean'];
+  deleteTodoStatus: Scalars['Boolean'];
+  updateTodo: Todo;
+  updateTodoStatus: TodoStatus;
+};
+
+
+export type MutationCreateTodoArgs = {
+  input: CreateTodoInput;
+};
+
+
+export type MutationCreateTodoStatusArgs = {
+  input: CreateTodoStatusInput;
+};
+
+
+export type MutationDeleteTodoArgs = {
+  id: Scalars['Int'];
+};
+
+
+export type MutationDeleteTodoStatusArgs = {
+  id: Scalars['Int'];
+};
+
+
+export type MutationUpdateTodoArgs = {
+  input: UpdateTodoInput;
+};
+
+
+export type MutationUpdateTodoStatusArgs = {
+  input: UpdateTodoStatusInput;
+};
+
+export type Node = {
+  id: Scalars['ID'];
+};
+
+export type Pagination = {
+  nodes: Array<Node>;
+  pageInfo: PaginationInfo;
+};
+
+export type PaginationInfo = {
+  __typename?: 'PaginationInfo';
+  count: Scalars['Int'];
+  hasNextPage: Scalars['Boolean'];
+  hasPreviousPage: Scalars['Boolean'];
+  page: Scalars['Int'];
+  paginationLength: Scalars['Int'];
+  totalCount: Scalars['Int'];
 };
 
 export type Query = {
   __typename?: 'Query';
-  todos: Array<Todo>;
-  users: Array<User>;
+  todo?: Maybe<Todo>;
+  todoPagination: TodoPagination;
+  todoStatus?: Maybe<TodoStatus>;
+  todos?: Maybe<Array<Maybe<Todo>>>;
+  user?: Maybe<User>;
 };
 
-export type Todo = {
+
+export type QueryTodoArgs = {
+  id?: InputMaybe<Scalars['Int']>;
+};
+
+
+export type QueryTodoPaginationArgs = {
+  limit: Scalars['Int'];
+  offset?: InputMaybe<Scalars['Int']>;
+};
+
+
+export type QueryTodoStatusArgs = {
+  id?: InputMaybe<Scalars['Int']>;
+};
+
+
+export type QueryUserArgs = {
+  id?: InputMaybe<Scalars['Int']>;
+};
+
+export type Todo = Node & {
   __typename?: 'Todo';
   id: Scalars['ID'];
   todo: Scalars['String'];
-  todoStatusId: Scalars['String'];
+  todoStatusId: Scalars['Int'];
   user: User;
+};
+
+export type TodoPagination = Pagination & {
+  __typename?: 'TodoPagination';
+  nodes: Array<Todo>;
+  pageInfo: PaginationInfo;
+};
+
+export type TodoStatus = Node & {
+  __typename?: 'TodoStatus';
+  id: Scalars['ID'];
+  status: Scalars['String'];
+};
+
+export type UpdateTodoInput = {
+  id: Scalars['Int'];
+  todo: Scalars['String'];
+  todoStatusId: Scalars['Int'];
+  userId: Scalars['Int'];
+};
+
+export type UpdateTodoStatusInput = {
+  id: Scalars['Int'];
+  todoStatus: Scalars['String'];
 };
 
 export type User = {
@@ -38,7 +156,7 @@ export type User = {
 export type TodoListQueryVariables = Exact<{ [key: string]: never; }>;
 
 
-export type TodoListQuery = { __typename?: 'Query', todos: Array<{ __typename?: 'Todo', id: string, todo: string, todoStatusId: string, user: { __typename?: 'User', id: string, name: string } }> };
+export type TodoListQuery = { __typename?: 'Query', todos?: Array<{ __typename?: 'Todo', id: string, todo: string, todoStatusId: number, user: { __typename?: 'User', id: string, name: string } } | null> | null };
 
 
 export const TodoListDocument = gql`
@@ -64,15 +182,188 @@ export default {
     "queryType": {
       "name": "Query"
     },
-    "mutationType": null,
+    "mutationType": {
+      "name": "Mutation"
+    },
     "subscriptionType": null,
     "types": [
       {
         "kind": "OBJECT",
-        "name": "Query",
+        "name": "Mutation",
         "fields": [
           {
-            "name": "todos",
+            "name": "createTodo",
+            "type": {
+              "kind": "NON_NULL",
+              "ofType": {
+                "kind": "OBJECT",
+                "name": "Todo",
+                "ofType": null
+              }
+            },
+            "args": [
+              {
+                "name": "input",
+                "type": {
+                  "kind": "NON_NULL",
+                  "ofType": {
+                    "kind": "SCALAR",
+                    "name": "Any"
+                  }
+                }
+              }
+            ]
+          },
+          {
+            "name": "createTodoStatus",
+            "type": {
+              "kind": "NON_NULL",
+              "ofType": {
+                "kind": "OBJECT",
+                "name": "TodoStatus",
+                "ofType": null
+              }
+            },
+            "args": [
+              {
+                "name": "input",
+                "type": {
+                  "kind": "NON_NULL",
+                  "ofType": {
+                    "kind": "SCALAR",
+                    "name": "Any"
+                  }
+                }
+              }
+            ]
+          },
+          {
+            "name": "deleteTodo",
+            "type": {
+              "kind": "NON_NULL",
+              "ofType": {
+                "kind": "SCALAR",
+                "name": "Any"
+              }
+            },
+            "args": [
+              {
+                "name": "id",
+                "type": {
+                  "kind": "NON_NULL",
+                  "ofType": {
+                    "kind": "SCALAR",
+                    "name": "Any"
+                  }
+                }
+              }
+            ]
+          },
+          {
+            "name": "deleteTodoStatus",
+            "type": {
+              "kind": "NON_NULL",
+              "ofType": {
+                "kind": "SCALAR",
+                "name": "Any"
+              }
+            },
+            "args": [
+              {
+                "name": "id",
+                "type": {
+                  "kind": "NON_NULL",
+                  "ofType": {
+                    "kind": "SCALAR",
+                    "name": "Any"
+                  }
+                }
+              }
+            ]
+          },
+          {
+            "name": "updateTodo",
+            "type": {
+              "kind": "NON_NULL",
+              "ofType": {
+                "kind": "OBJECT",
+                "name": "Todo",
+                "ofType": null
+              }
+            },
+            "args": [
+              {
+                "name": "input",
+                "type": {
+                  "kind": "NON_NULL",
+                  "ofType": {
+                    "kind": "SCALAR",
+                    "name": "Any"
+                  }
+                }
+              }
+            ]
+          },
+          {
+            "name": "updateTodoStatus",
+            "type": {
+              "kind": "NON_NULL",
+              "ofType": {
+                "kind": "OBJECT",
+                "name": "TodoStatus",
+                "ofType": null
+              }
+            },
+            "args": [
+              {
+                "name": "input",
+                "type": {
+                  "kind": "NON_NULL",
+                  "ofType": {
+                    "kind": "SCALAR",
+                    "name": "Any"
+                  }
+                }
+              }
+            ]
+          }
+        ],
+        "interfaces": []
+      },
+      {
+        "kind": "INTERFACE",
+        "name": "Node",
+        "fields": [
+          {
+            "name": "id",
+            "type": {
+              "kind": "NON_NULL",
+              "ofType": {
+                "kind": "SCALAR",
+                "name": "Any"
+              }
+            },
+            "args": []
+          }
+        ],
+        "interfaces": [],
+        "possibleTypes": [
+          {
+            "kind": "OBJECT",
+            "name": "Todo"
+          },
+          {
+            "kind": "OBJECT",
+            "name": "TodoStatus"
+          }
+        ]
+      },
+      {
+        "kind": "INTERFACE",
+        "name": "Pagination",
+        "fields": [
+          {
+            "name": "nodes",
             "type": {
               "kind": "NON_NULL",
               "ofType": {
@@ -80,8 +371,8 @@ export default {
                 "ofType": {
                   "kind": "NON_NULL",
                   "ofType": {
-                    "kind": "OBJECT",
-                    "name": "Todo",
+                    "kind": "INTERFACE",
+                    "name": "Node",
                     "ofType": null
                   }
                 }
@@ -90,22 +381,195 @@ export default {
             "args": []
           },
           {
-            "name": "users",
+            "name": "pageInfo",
             "type": {
               "kind": "NON_NULL",
               "ofType": {
-                "kind": "LIST",
-                "ofType": {
-                  "kind": "NON_NULL",
-                  "ofType": {
-                    "kind": "OBJECT",
-                    "name": "User",
-                    "ofType": null
-                  }
-                }
+                "kind": "OBJECT",
+                "name": "PaginationInfo",
+                "ofType": null
               }
             },
             "args": []
+          }
+        ],
+        "interfaces": [],
+        "possibleTypes": [
+          {
+            "kind": "OBJECT",
+            "name": "TodoPagination"
+          }
+        ]
+      },
+      {
+        "kind": "OBJECT",
+        "name": "PaginationInfo",
+        "fields": [
+          {
+            "name": "count",
+            "type": {
+              "kind": "NON_NULL",
+              "ofType": {
+                "kind": "SCALAR",
+                "name": "Any"
+              }
+            },
+            "args": []
+          },
+          {
+            "name": "hasNextPage",
+            "type": {
+              "kind": "NON_NULL",
+              "ofType": {
+                "kind": "SCALAR",
+                "name": "Any"
+              }
+            },
+            "args": []
+          },
+          {
+            "name": "hasPreviousPage",
+            "type": {
+              "kind": "NON_NULL",
+              "ofType": {
+                "kind": "SCALAR",
+                "name": "Any"
+              }
+            },
+            "args": []
+          },
+          {
+            "name": "page",
+            "type": {
+              "kind": "NON_NULL",
+              "ofType": {
+                "kind": "SCALAR",
+                "name": "Any"
+              }
+            },
+            "args": []
+          },
+          {
+            "name": "paginationLength",
+            "type": {
+              "kind": "NON_NULL",
+              "ofType": {
+                "kind": "SCALAR",
+                "name": "Any"
+              }
+            },
+            "args": []
+          },
+          {
+            "name": "totalCount",
+            "type": {
+              "kind": "NON_NULL",
+              "ofType": {
+                "kind": "SCALAR",
+                "name": "Any"
+              }
+            },
+            "args": []
+          }
+        ],
+        "interfaces": []
+      },
+      {
+        "kind": "OBJECT",
+        "name": "Query",
+        "fields": [
+          {
+            "name": "todo",
+            "type": {
+              "kind": "OBJECT",
+              "name": "Todo",
+              "ofType": null
+            },
+            "args": [
+              {
+                "name": "id",
+                "type": {
+                  "kind": "SCALAR",
+                  "name": "Any"
+                }
+              }
+            ]
+          },
+          {
+            "name": "todoPagination",
+            "type": {
+              "kind": "NON_NULL",
+              "ofType": {
+                "kind": "OBJECT",
+                "name": "TodoPagination",
+                "ofType": null
+              }
+            },
+            "args": [
+              {
+                "name": "limit",
+                "type": {
+                  "kind": "NON_NULL",
+                  "ofType": {
+                    "kind": "SCALAR",
+                    "name": "Any"
+                  }
+                }
+              },
+              {
+                "name": "offset",
+                "type": {
+                  "kind": "SCALAR",
+                  "name": "Any"
+                }
+              }
+            ]
+          },
+          {
+            "name": "todoStatus",
+            "type": {
+              "kind": "OBJECT",
+              "name": "TodoStatus",
+              "ofType": null
+            },
+            "args": [
+              {
+                "name": "id",
+                "type": {
+                  "kind": "SCALAR",
+                  "name": "Any"
+                }
+              }
+            ]
+          },
+          {
+            "name": "todos",
+            "type": {
+              "kind": "LIST",
+              "ofType": {
+                "kind": "OBJECT",
+                "name": "Todo",
+                "ofType": null
+              }
+            },
+            "args": []
+          },
+          {
+            "name": "user",
+            "type": {
+              "kind": "OBJECT",
+              "name": "User",
+              "ofType": null
+            },
+            "args": [
+              {
+                "name": "id",
+                "type": {
+                  "kind": "SCALAR",
+                  "name": "Any"
+                }
+              }
+            ]
           }
         ],
         "interfaces": []
@@ -160,7 +624,88 @@ export default {
             "args": []
           }
         ],
-        "interfaces": []
+        "interfaces": [
+          {
+            "kind": "INTERFACE",
+            "name": "Node"
+          }
+        ]
+      },
+      {
+        "kind": "OBJECT",
+        "name": "TodoPagination",
+        "fields": [
+          {
+            "name": "nodes",
+            "type": {
+              "kind": "NON_NULL",
+              "ofType": {
+                "kind": "LIST",
+                "ofType": {
+                  "kind": "NON_NULL",
+                  "ofType": {
+                    "kind": "OBJECT",
+                    "name": "Todo",
+                    "ofType": null
+                  }
+                }
+              }
+            },
+            "args": []
+          },
+          {
+            "name": "pageInfo",
+            "type": {
+              "kind": "NON_NULL",
+              "ofType": {
+                "kind": "OBJECT",
+                "name": "PaginationInfo",
+                "ofType": null
+              }
+            },
+            "args": []
+          }
+        ],
+        "interfaces": [
+          {
+            "kind": "INTERFACE",
+            "name": "Pagination"
+          }
+        ]
+      },
+      {
+        "kind": "OBJECT",
+        "name": "TodoStatus",
+        "fields": [
+          {
+            "name": "id",
+            "type": {
+              "kind": "NON_NULL",
+              "ofType": {
+                "kind": "SCALAR",
+                "name": "Any"
+              }
+            },
+            "args": []
+          },
+          {
+            "name": "status",
+            "type": {
+              "kind": "NON_NULL",
+              "ofType": {
+                "kind": "SCALAR",
+                "name": "Any"
+              }
+            },
+            "args": []
+          }
+        ],
+        "interfaces": [
+          {
+            "kind": "INTERFACE",
+            "name": "Node"
+          }
+        ]
       },
       {
         "kind": "OBJECT",
